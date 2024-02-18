42ca992ccaaa75304dd946dafb75e93ae902833a
dwkiller <274779798@qq,com>
2024-02-18 18:32:58 +0800

    a

--- mimi-manager/src/main/java/com/mimi/config/GlobalExceptionHandler.java
+++ mimi-manager/src/main/java/com/mimi/config/GlobalExceptionHandler.java
@@ -23,48 +23,15 @@ import java.util.List;
 @RestControllerAdvice("com.mimi")
 public class GlobalExceptionHandler {
 
-    /**
-     * 忽略参数异常处理器
-     *
-     * @param e 忽略参数异常
-     * @return ResponseResult
-     */
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MissingServletRequestParameterException.class)
-    public R parameterMissingExceptionHandler(MissingServletRequestParameterException e) {
-        log.error("", e);
-        return R.error("请求参数 " + e.getParameterName() + " 不能为空");
-    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MimiException.class)
-    public R parameterMimiExceptionHandler(MimiException e) {
-        log.error("", e);
-        return R.error(e.getMessage());
-    }
-
-    /**
-     * 参数效验异常处理器
-     *
-     * @param e 参数验证异常
-     * @return ResponseInfo
-     */
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public R parameterExceptionHandler(MethodArgumentNotValidException e) {
-        log.error("", e);
-        // 获取异常信息
-        BindingResult exceptions = e.getBindingResult();
-        // 判断异常中是否有错误信息，如果存在就使用异常中的消息，否则使用默认消息
-        if (exceptions.hasErrors()) {
-            List<ObjectError> errors = exceptions.getAllErrors();
-            if (!errors.isEmpty()) {
-                // 这里列出了全部错误参数，按正常逻辑，只需要第一条错误即可
-                FieldError fieldError = (FieldError) errors.get(0);
-                return R.error( fieldError.getDefaultMessage());
-//                return new ResponseResult(ResultEnum.PARAMETER_ERROR.getCode(), fieldError.getDefaultMessage());
-            }
+    @ExceptionHandler(value = Throwable.class)
+    public R<?> exceptionHandler(Throwable throwable){
+        throwable.printStackTrace();
+        String message = throwable.getMessage();
+        Throwable innerThrowable = throwable.getCause();
+        if(innerThrowable!=null){
+            message = innerThrowable.getCause()!=null?innerThrowable.getCause().getMessage():innerThrowable.getMessage();
         }
-        return R.error("参数异常");
+        return R.error(message);
     }
+
 }
