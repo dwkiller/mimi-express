c5afea085c9abdb2840dcc750b8e5e5f42a9a002
dwkiller <274779798@qq,com>
2024-02-25 11:07:02 +0800

    a

--- mimi-manager/src/main/java/com/mimi/ExpressApplication.java
+++ mimi-manager/src/main/java/com/mimi/ExpressApplication.java
@@ -1,6 +1,10 @@
 package com.mimi;
 
+import com.baomidou.mybatisplus.core.MybatisConfiguration;
+import com.baomidou.mybatisplus.core.metadata.TableInfoHelper;
+import com.mimi.core.express.entity.order.BaseOrder;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.builder.MapperBuilderAssistant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.oas.annotations.EnableOpenApi;
@@ -11,6 +15,7 @@ import springfox.documentation.oas.annotations.EnableOpenApi;
 public class ExpressApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExpressApplication.class,args);
+        TableInfoHelper.initTableInfo(new MapperBuilderAssistant(new MybatisConfiguration(), ""), BaseOrder.class);
         log.info("项目启动成功...");
     }
 }
